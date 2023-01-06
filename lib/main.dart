9b0d0d93a42c6be195ace09c170a12c3c55603cc
MSCosamaShallal <osamafalah85@yahoo.com>
2023-01-06 21:29:24 +0300

    new commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
+import 'mycomponents/homepage.dart';
 import 'package:flutter/material.dart';
 
-import 'HomeScreen.dart';
 
-void main(){
-  runApp(MyApp());
-}
+
+void main()=> runApp(const MyApp()) ;
+
 
 
 class MyApp extends StatelessWidget {
@@ -12,10 +12,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        home:HomeScreen() ,
+    return const  MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
     );
   }
 }
-
-
